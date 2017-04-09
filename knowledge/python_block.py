8951402de37202ecbc4b7392c01d9490e9b9f7b6
doktorspaceman <beyondwrinkled@gmail.com>
2017-04-09 15:34:27 -0700

    finished hacky pygmentizing of python code

--- knowledge/python_block.py
+++ knowledge/python_block.py
@@ -1,14 +1,26 @@
 import numpy as np
+import os
+import subprocess
 
 class PythonBlock(MemoryBlock):
 
-	def __init__(self, content_q, content_a, importance=1.0):
-		MemoryBlock.__init__(self, content_q, content_a, priority=1.2)
+	def __init__(self, question, response, priority=1.2):
+		MemoryBlock.__init__(self, question, response, priority=priority)
 
 	def show_question(self):
 		# pygmentize
-		print self.question
+		self.__pygmentizit(self.question)
 
 	def show_response(self):
 		# pygmentize
-		print self.response
+		self.__pygmentizit(self.response)
+
+	def __pygmentizit(self, content, tmp_dir='tmpPythonBlock', tmp_pyc='tmp.py'):
+		tmp_directory = tmp_dir
+		tmp_pyth_file = tmp_dir + tmp_pyc
+		os.mkdir(tmp_directory)
+		with open(tmp_pyth_file, 'wb') as f:
+			f.write(content)
+		_ = subprocess.call(['pygmentize', tmp_pyth_file])
+		os.remove(tmp_pyth_file)
+		os.rmdir(tmp_directory)
